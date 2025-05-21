d0f1f3a546df3a7781220710ebf848388fe2a8e1
priya4565 <93039352+priya4565@users.noreply.github.com>
2025-05-21 15:59:42 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+def main():
+  print("hello, github actions!")
+  print("this is a simple python script")
+  print("it runs as part of an automated workflow")
+
+if __name__ == "__main__":
+  main()
